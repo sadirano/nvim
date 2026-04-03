706ba2d139624dfd80fa3fb0f4233fe292801caf
Sadirano <leonardofariasbona@gmail.com>
2026-04-02 23:03:50 -0300

    feat: LSP and completion

---
+++ lua/sadirano/plugins/lsp/lspconfig.lua
@@ -0,0 +1,62 @@
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+        -- Keymaps set on every LSP attach
+        vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function(ev)
+                local opts = { buffer = ev.buf, remap = false }
+                local keymap = vim.keymap
+
+                keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+                keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+                keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+                keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+                keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+                keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+                keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+                keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+                keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+                keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+            end,
+        })
+
+        -- Diagnostic display
+        vim.diagnostic.config({
+            virtual_text = true,
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = "E",
+                    [vim.diagnostic.severity.WARN] = "W",
+                    [vim.diagnostic.severity.HINT] = "H",
+                    [vim.diagnostic.severity.INFO] = "I",
+                },
+            },
+        })
+
+        -- Shared capabilities for all servers
+        vim.lsp.config("*", {
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+        })
+
+        -- Per-server overrides
+        vim.lsp.config("lua_ls", {
+            settings = {
+                Lua = {
+                    diagnostics = { globals = { "vim" } },
+                },
+            },
+        })
+
+        -- Enable servers (nvim-lspconfig provides default cmd/filetypes/root_dir)
+        vim.lsp.enable({ "lua_ls", "rust_analyzer", "ts_ls" })
+    end,
+}
