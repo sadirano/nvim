84b9fada149db6905c48ef5c35cdb9dff292e7c7
Sadirano <leonardofariasbona@gmail.com>
2026-04-02 23:03:43 -0300

    feat: navigation — telescope, commenting, autopairs, surround

---
+++ lua/sadirano/plugins/telescope.lua
@@ -0,0 +1,42 @@
+return {
+    {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = vim.fn.has("win32") == 1
+            and "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+            or "make",
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope-fzf-native.nvim",
+            "nvim-telescope/telescope-ui-select.nvim",
+        },
+        config = function()
+            local telescope = require("telescope")
+            local actions = require("telescope.actions")
+            local themes = require("telescope.themes")
+
+            telescope.setup({
+                defaults = {
+                    mappings = {
+                        i = {
+                            ["<C-k>"] = actions.move_selection_previous,
+                            ["<C-j>"] = actions.move_selection_next,
+                            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                        },
+                    },
+                },
+                extensions = {
+                    ["ui-select"] = {
+                        themes.get_dropdown({}),
+                    },
+                },
+            })
+
+            telescope.load_extension("fzf")
+            telescope.load_extension("ui-select")
+        end,
+    },
+}
