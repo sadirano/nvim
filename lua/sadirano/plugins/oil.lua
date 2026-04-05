2f8149b15f0f2cb9ff93a65375adcf3194a36e6d
Sadirano <leonardofariasbona@gmail.com>
2026-04-05 12:56:42 -0300

    fix: Adjusted oil behaviour to my preference

--- lua/sadirano/plugins/oil.lua
+++ lua/sadirano/plugins/oil.lua
@@ -1,53 +1,51 @@
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "oil",
-    callback = function()
-        vim.opt_local.colorcolumn = ""
-    end,
+  pattern = "oil",
+  callback = function()
+    vim.opt_local.colorcolumn = ""
+  end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "oil_preview",
-    callback = function(params)
-        vim.keymap.set("n", "<CR>", function()
-            require("oil").open()
-        end, { buffer = params.buf, nowait = true })
-    end,
+  pattern = "oil_preview",
+  callback = function(params)
+    vim.keymap.set("n", "<CR>", "o", { buffer = params.buf, remap = true, nowait = true })
+  end,
 })
 
 return {
+  {
     "stevearc/oil.nvim",
+    opts = {},
+    -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require("oil").setup({
-            float = {
-                relative = "editor",
-                width = 0.8,
-                height = 0.8,
-                border = "rounded",
-            },
-            confirmation = {
-                border = "rounded",
-            },
-            keymaps = {
-                ["g?"] = "actions.show_help",
-                ["<CR>"] = "actions.select",
-                ["<C-\\>"] = "actions.select_vsplit",
-                ["<C-s>"] = "actions.select_split",
-                ["<C-t>"] = "actions.select_tab",
-                ["<C-p>"] = "actions.preview",
-                ["<C-c>"] = "actions.close",
-                ["q"] = "actions.close",
-                ["<C-r>"] = "actions.refresh",
-                ["<BS>"] = "actions.parent",
-                ["-"] = "actions.parent",
-                ["_"] = "actions.open_cwd",
-                ["`"] = "actions.cd",
-                ["~"] = "actions.tcd",
-                ["gs"] = "actions.change_sort",
-                ["gx"] = "actions.open_external",
-                ["g."] = "actions.toggle_hidden",
-            },
-            use_default_keymaps = false,
-        })
+      require("oil").setup({
+        keymaps = {
+          ["g?"] = "actions.show_help",
+          ["<CR>"] = "actions.select",
+          ["<C-\\>"] = "actions.select_vsplit",
+          ["<C-enter>"] = "actions.select_split", -- this is used to navigate left
+          ["<C-t>"] = "actions.select_tab",
+          ["<C-p>"] = "actions.preview",
+          ["<C-c>"] = "actions.close",
+          ["q"] = "actions.close",
+          ["<C-r>"] = "actions.refresh",
+          ["<backspace>"] = "actions.parent",
+          ["-"] = "actions.parent",
+          ["_"] = "actions.open_cwd",
+          ["``"] = "actions.cd",
+          ["~~"] = "actions.tcd",
+          ["gs"] = "actions.change_sort",
+          ["gx"] = "actions.open_external",
+          ["g."] = "actions.toggle_hidden",
+        },
+        confirmation = {
+          border = "rounded",
+          win_options = { winblend = 0 },
+        },
+        is_git_dirty = true,
+        use_default_keymaps = false,
+      })
     end,
+  },
 }
