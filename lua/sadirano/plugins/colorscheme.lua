2dd896b4a1fd36897c7ba1bf25fad0ecd98a42de
Sadirano <leonardofariasbona@gmail.com>
2026-04-05 12:55:09 -0300

    feat: Added transparency to theme

--- lua/sadirano/plugins/colorscheme.lua
+++ lua/sadirano/plugins/colorscheme.lua
@@ -2,6 +2,13 @@ return {
     "folke/tokyonight.nvim",
     priority = 1000,
     config = function()
+        require("tokyonight").setup({
+            transparent = true,
+            styles = {
+                sidebars = "transparent",
+                floats = "transparent",
+            },
+        })
         vim.cmd("colorscheme tokyonight")
     end,
 }
