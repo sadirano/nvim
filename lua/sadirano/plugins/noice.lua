3c332e46a925044256f62144d2925bcefe06406c
Sadirano <leonardofariasbona@gmail.com>
2026-04-29 01:46:19 -0300

    feat: add noice.nvim as optional UI plugin, disable fidget

---
+++ lua/sadirano/plugins/noice.lua
@@ -0,0 +1,27 @@
+local ok, local_cfg = pcall(require, "sadirano.local.profiles")
+local enabled = ok and local_cfg.ui and local_cfg.ui.noice == true
+
+return {
+    "folke/noice.nvim",
+    enabled = enabled,
+    event = "VeryLazy",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+    },
+    config = function()
+        require("noice").setup({
+            lsp = {
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true,
+                },
+            },
+            presets = {
+                long_message_to_split = true,
+                lsp_doc_border = true,
+            },
+        })
+    end,
+}
