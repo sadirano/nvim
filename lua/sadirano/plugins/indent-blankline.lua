1f528525e01d8e16049aa391304a5597a15d4f11
Sadirano <leonardofariasbona@gmail.com>
2026-04-29 02:19:21 -0300

    feat: make supermaven and indent-blankline optional, remove dead plugin stubs

--- lua/sadirano/plugins/indent-blankline.lua
+++ lua/sadirano/plugins/indent-blankline.lua
@@ -1,5 +1,9 @@
+local ok, local_cfg = pcall(require, "sadirano.local.profiles")
+local enabled = ok and local_cfg.ui and local_cfg.ui.indent_guides == true
+
 return {
     "lukas-reineke/indent-blankline.nvim",
+    enabled = enabled,
     main = "ibl",
     event = { "BufReadPre", "BufNewFile" },
     opts = {},
--- lua/sadirano/plugins/supermaven.lua
+++ lua/sadirano/plugins/supermaven.lua
@@ -1,9 +1,11 @@
+local ok, local_cfg = pcall(require, "sadirano.local.profiles")
+local enabled = ok and local_cfg.ui and local_cfg.ui.supermaven == true
+
 return {
-  {
     "supermaven-inc/supermaven-nvim",
+    enabled = enabled,
     event = "InsertEnter",
     config = function()
-      require("supermaven-nvim").setup({})
+        require("supermaven-nvim").setup({})
     end,
-  },
 }
