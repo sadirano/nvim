f9d0918d199c0657167793c7eb1fdbfdc0e41f1f
Sadirano <leonardofariasbona@gmail.com>
2026-07-12 01:05:41 -0300

    chore(treesitter): explicitly set branch to master to force lazy to switch branches

--- lua/sadirano/plugins/treesitter.lua
+++ lua/sadirano/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        branch = "master",
         build = ":TSUpdate",
         lazy = false,
         config = function()
@@ -45,6 +46,7 @@ return {
     },
     {
         "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = "master",
         dependencies = { "nvim-treesitter/nvim-treesitter" },
         config = function()
             require("nvim-treesitter-textobjects").setup({
